792b26aa353396dc5dc5fb09dce0901dd75a6a6a
SeanPrice13 <bentleyprice@live.com>
2021-07-04 14:30:32 -0400

    Added certificates +
    certs and a basic project added

---
+++ RockPaperScissors/js/app.js
@@ -0,0 +1,103 @@
+// Variables
+const rock = document.getElementById('player-section').firstElementChild,
+    paper = rock.nextElementSibling,
+    scissors = paper.nextElementSibling,
+    comPlayer = document.getElementById('CPU-section').firstElementChild,
+    comChoice = [rock.src, paper.src, scissors.src];
+let wins = 0,
+    losses = 0,
+    ties = 0,
+    round = 1;
+
+//Event Listeners
+rock.addEventListener('click', rockChoice);
+paper.addEventListener('click', paperChoice);
+scissors.addEventListener('click', scissorsChoice);
+
+function rockChoice() {
+    let rand = comChoice[Math.floor(Math.random() * comChoice.length)];
+    comPlayer.src = rand;
+    paper.style.display = 'none';
+    scissors.style.display = 'none';
+    if (comPlayer.src == rock.src) {
+        rock.parentElement.setAttribute('id', 'tie');
+        comPlayer.parentElement.setAttribute('id', 'tie');
+        document.getElementById('status').innerText = 'It\'s a Tie!';
+        ties++;
+    } else if (comPlayer.src == paper.src) {
+        rock.parentElement.setAttribute('id', 'lose');
+        comPlayer.parentElement.setAttribute('id', 'win');
+        document.getElementById('status').innerText = 'Sorry, You Lost.';
+        losses++;
+    } else {
+        rock.parentElement.setAttribute('id', 'win');
+        comPlayer.parentElement.setAttribute('id', 'lose');
+        document.getElementById('status').innerText = 'You Won!';
+        wins++;
+    }
+    newRound();
+}
+
+function paperChoice() {
+    let rand = comChoice[Math.floor(Math.random() * comChoice.length)];
+    comPlayer.src = rand;
+    rock.style.display = 'none';
+    scissors.style.display = 'none';
+    if (comPlayer.src == paper.src) {
+        rock.parentElement.setAttribute('id', 'tie');
+        comPlayer.parentElement.setAttribute('id', 'tie');
+        document.getElementById('status').innerText = 'It\'s a Tie!';
+        ties++;
+    } else if (comPlayer.src == scissors.src) {
+        rock.parentElement.setAttribute('id', 'lose');
+        comPlayer.parentElement.setAttribute('id', 'win');
+        document.getElementById('status').innerText = 'Sorry, You Lost.';
+        losses++;
+    } else {
+        rock.parentElement.setAttribute('id', 'win');
+        comPlayer.parentElement.setAttribute('id', 'lose');
+        document.getElementById('status').innerText = 'You Won!';
+        wins++;
+    }
+    newRound();
+}
+
+function scissorsChoice() {
+    let rand = comChoice[Math.floor(Math.random() * comChoice.length)];
+    comPlayer.src = rand;
+    paper.style.display = 'none';
+    rock.style.display = 'none';
+    if (comPlayer.src == scissors.src) {
+        rock.parentElement.setAttribute('id', 'tie');
+        comPlayer.parentElement.setAttribute('id', 'tie');
+        document.getElementById('status').innerText = 'It\'s a Tie!';
+        ties++;
+    } else if (comPlayer.src == rock.src) {
+        rock.parentElement.setAttribute('id', 'lose');
+        comPlayer.parentElement.setAttribute('id', 'win');
+        document.getElementById('status').innerText = 'Sorry, You Lost.';
+        losses++;
+    } else {
+        rock.parentElement.setAttribute('id', 'win');
+        comPlayer.parentElement.setAttribute('id', 'lose');
+        document.getElementById('status').innerText = 'You Won!';
+        wins++;
+    }
+    newRound();
+}
+
+function newRound() {
+    setTimeout(() => {
+        document.getElementById('round').innerText = 'Game ' + round++;
+        document.getElementById('status').innerText = 'Wins: ' + wins + ' Losses: ' + losses + ' Ties: ' + ties;
+        rock.parentElement.setAttribute('id', 'player-section');
+        comPlayer.parentElement.setAttribute('id', 'CPU-section');
+        paper.style.display = 'inline';
+        rock.style.display = 'inline';
+        scissors.style.display = 'inline';
+        comPlayer.src = rock.src;
+    }, 1000);
+}
+
+document.getElementById('round').innerText = 'Game ' + round++;
+document.getElementById('status').innerText = 'Pick a hand.';
--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const mobileBtn = document.getElementById('mobile-cta')
-              nav = document.querySelector('nav')
-              mobileBtnExit = document.getElementById('mobile-exit');
+const oops = document.getElementById('mobile-cta'),
+    nav = document.querySelector('nav'),
+    mobileBtnExit = document.getElementById('mobile-exit'),
+    rpsBtn = document.getElementById('rps-button');
 
-        mobileBtn.addEventListener('click', () => {
-            nav.classList.add('menu-btn');
-        })
+oops.addEventListener('click', () => {
+    nav.classList.add('menu-btn');
+});
 
-        mobileBtnExit.addEventListener('click', () => {
-            nav.classList.remove('menu-btn');
-        })
+mobileBtnExit.addEventListener('click', () => {
+    nav.classList.remove('menu-btn');
+});
+
+rpsBtn.addEventListener('click', () => {
+    if (document.getElementById('rps-game').style.display !== 'block') {
+        document.getElementById('rps-game').style.display = 'block';
+    } else {
+        document.getElementById('rps-game').style.display = 'none';
+    }
+});
